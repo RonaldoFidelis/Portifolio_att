01e1b92248be0b8ec74b7dc0cd689e181f7b4def
RonaldoFidelis <ronaldofidelis.ti@gmail.com>
2023-01-18 11:30:21 -0300

    feat:AddSkillsFinalize

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@ const navMenu = document.querySelector("#nav-menu");
 const navToggle = document.querySelector("#nav-toggle");
 const navClose = document.querySelector("#nav-close");
 
-
 // Função para abrir/fechar menu de navegação
 if(navToggle){
     navToggle.addEventListener('click', () => {
@@ -15,3 +14,22 @@ if(navClose){
         navMenu.classList.remove('open-menu')
     })
 }
+
+// abri e fechar o menu de skills
+const skillsContent = document.getElementsByClassName('skills_content');
+const skillsHeader = document.querySelectorAll('.skills_header');
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills_content skills_close'
+    }
+    if(itemClass === 'skills_content skills_close'){
+        this.parentNode.className = 'skills_content skills_open'
+    }
+}
+
+skillsHeader.forEach(el => {
+    el.addEventListener('click', toggleSkills);
+});
